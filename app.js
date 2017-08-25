5a1bcf9adc9d7a87ee5bf9e2ca9fa6db77000f4a
seunzone <darealseun@gmail.com>
2017-08-25 11:45:12 +0100

    Getting started with Semantic UI

--- app.js
+++ app.js
@@ -21,8 +21,29 @@ var blogSchema = new mongoose.Schema({
 });
 var Blog = mongoose.model("Blog", blogSchema);
 
+// Blog.create({
+// 	title: "Test Title Blog",
+// 	image: "https://scontent.flos3-1.fna.fbcdn.net/v/t1.0-1/p160x160/20767815_1577821268935675_1650698914966330585_n.jpg?_nc_eui2=v1%3AAeFu6tl94dolWmOLddc6bBX126ntjmrFBRbMOSPLhqaJkNJBzr-GZotQhnAazP5DSbdqRcsHm5ZFO24epDfeBByUmYftqfsBHjGkanAfOQ47Gw&oh=20c5bb7fe50a75b4bc2f88cc9e6781e8&oe=5A37CF0D",
+// 	body: "Type Blog Post Here"
+// });
 
 //ROUTES
+
+app.get("/", function(req, res){
+	res.redirect("/blog");
+});
+
+
+app.get("/blog", function(req,res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log("ERROR!!");
+		} else{
+			res.render("index", {blogs:blogs});
+		}
+	});
+	
+});
 
 app.listen(3000, function () {
   console.log('Our Site is on Port 3000');
